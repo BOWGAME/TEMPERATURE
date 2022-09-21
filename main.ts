a7c72279e36995c09a952bbaf9c8766f86585d86
BOWGAME <113489307+BOWGAME@users.noreply.github.com>
2022-09-21 11:59:29 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,27 +1,105 @@
+function Face_Recognition () {
+    huskylens.initI2c()
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+    while (초기화 == 0) {
+        huskylens.request()
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showLeds(`
+                . # . # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+            초기화 = 1
+        } else {
+            초기화 = 0
+        }
+    }
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    basic.showString("HI!")
+    basic.pause(100)
+    기분 = 0
+}
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(min)
-    basic.showString("'C")
-    basic.clearScreen()
+    Tag_Recognition()
+})
+function Object_Classification () {
+	
+}
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    기분 += -1
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(max)
-    basic.showString("'C")
-    basic.clearScreen()
+    기분 = 0
+    초기화 = 0
+    Face_Recognition()
+    Tag_Recognition()
+    Object_Classification()
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("GAMEBOY")
 })
-let min = 0
-let max = 0
-let currenttemperature = input.temperature()
-max = currenttemperature
-min = currenttemperature
-basic.forever(function () {
-    basic.showString(".")
-    currenttemperature = input.temperature()
-    if (currenttemperature < min) {
-        min = currenttemperature
-    } else if (currenttemperature > max) {
-        max = currenttemperature
+function Tag_Recognition () {
+    huskylens.initI2c()
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+    while (기분 < 100) {
+        huskylens.request()
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+            basic.pause(2000)
+            basic.clearScreen()
+            기분 += 1
+        }
     }
-    basic.pause(1000)
-    basic.clearScreen()
-    basic.pause(1000)
-})
+    if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+        basic.pause(2000)
+        basic.clearScreen()
+        기분 += 1
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        Object_Classification()
+    }
+}
+let 기분 = 0
+let 초기화 = 0
+Face_Recognition()
+Tag_Recognition()
+Object_Classification()
+basic.showLeds(`
+    . # # # .
+    # # # # #
+    # # # # #
+    . . # . #
+    # . # . .
+    `)
+basic.showString("GAMEBOY")
